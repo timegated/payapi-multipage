0a4096f99f908315c3e35beb5d3622bf79fd63a6
dbeccaria826 <beccdaniel@gmail.com>
2020-11-05 09:07:25 -0500

    Webpack: configuring multi-page support with webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: {
+    index: "./src/index/index.js",
+    about: "./src/about/index.js",
+    contact: "./src/contact/index.js",
+    pricing: "./src/pricing/index.js"
+  }
+}
